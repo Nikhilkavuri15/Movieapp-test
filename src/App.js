c60f7b8a806972706f8cac9d79da0626ece92fac
Nikhilkavuri15 <nikhil@crewscale.com>
2024-03-31 00:15:45 +0530

    Created the code sturcture

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  {
+    /* <img class="poster" src="https://image.tmdb.org/t/p/w500/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg" alt="Kung Fu Panda 4 Poster" />
+     */
+  }
+  return <div className="App"></div>;
+}
+
+export default App;
